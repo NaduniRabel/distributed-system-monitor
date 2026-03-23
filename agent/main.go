7caed91999220dfed0ee4e49530d431dfefd5685
Naduni Rabel <rabelnaduni2000@gmail.com>
2026-03-23 22:12:38 +0530

    Agent import update

--- agent/main.go
+++ agent/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"github.com/NaduniRabel/distributed-system-monitor/agent/collector"
+	//pb "github.com/NaduniRabel/distributed-system-monitor/proto"
 )
 
 func main() {
 
 	collector.CollectMetrics()
+	//pb.NewMetricServiceClient()
 
 }
